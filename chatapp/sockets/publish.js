16e07472a61e5692cc2ad9b6041329b882dd88a5
himekuri <himekuri9@gmail.com>
2021-11-05 14:01:31 +0900

    投稿機能の実装

--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -2,7 +2,12 @@
 
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
-    socket.on('', function (data) {
+    socket.on('sendMessageEvent', function (data) {
+        if (!data) {
+            return;
+        }
 
+        // 全クライアントが受信するメッセージ表示イベント（receiveMessageEvent）を送信する
+        io.sockets.emit('receiveMessageEvent', data);
     });
 };
